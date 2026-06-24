107a1c22375b2ad46e8c7bfee699618c7b8aa929
shreyansh2416 <81609539+shreyansh2416@users.noreply.github.com>
2026-06-24 00:39:44 +0000

    Bypass all build checks

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   typescript: {
-    // !! WARN !!
     // Dangerously allow production builds to successfully complete even if
     // your project has type errors.
     ignoreBuildErrors: true,
+  },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
   },
 };
 
